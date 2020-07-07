62406bf199e17d7e2eeefea1bd3e63f531ecdb2d
Andrii Nikitin <anikitin@suse.de>
2020-07-07 17:02:48 +0200

    Use non-blocking mirror checks during file render

--- lib/MirrorCache/WebAPI/Plugin/RenderFileFromMirror.pm
+++ lib/MirrorCache/WebAPI/Plugin/RenderFileFromMirror.pm
@@ -27,75 +27,60 @@ sub register {
     my ($self, $app) = @_;
  
     $app->helper( 'mirrorcache.render_file' => sub {
-        my ($c, $filepath) = @_;
    
+        my ($c, $filepath, $route) = @_;
+        $c->emit_event('mc_dispatch', $filepath);
         my $mirror = "";
-        eval {
-            $mirror = $c->mirrorcache->best_mirror( $filepath );
-            1;
-        } or $c->emit_event('mc_best_mirror_error', {path => $filepath, err => $@});
-        $c->emit_event('mc_debug', {path => $filepath, best_mirror => $mirror});
-
-        return $c->redirect_to($mirror) if $mirror;
-        
-        $c->emit_event('mc_debug', {path => $filepath, c => $c, d => 1});
-        return $c->mc->root->render_file($c, $filepath);
-    });
-
-    $app->helper( 'mirrorcache.best_mirror' => sub {
-        my ($c, $filepath) = @_;
         my $f = Mojo::File->new($filepath);
-    
         my $dirname  = $f->dirname;
         my $basename = $f->basename;
-        # TODO move these checks to Dir ?
+
         my $folder = $c->schema->resultset('Folder')->find({path => $dirname});
         unless ($folder) {
             $c->emit_event('mc_path_miss', $dirname);
-            return undef;
-        }
-        my $file   = $c->schema->resultset('File')->find({folder_id => $folder->id, name => $basename});
-        unless ($file) {
-            $c->emit_event('mc_path_miss', $dirname) unless $file;
-            return undef
+            return $c->mc->root->render_file($c, $filepath); # TODO we still can check file on mirrors even if it is missing
         }
 
+        my $file = $c->schema->resultset('File')->find({folder_id => $folder->id, name => $basename});
+        unless ($file) {
+            $c->emit_event('mc_path_miss', $dirname);
+            return $c->mc->root->render_file($c, $filepath); # TODO we still can check file on mirrors even if it is missing
+        }
+        $c->render_later;
         my $mirrors = $c->schema->resultset('Server')->mirrors_country($c->mmdb->country(), $folder->id, $basename);
         my $ua  = Mojo::UserAgent->new;
-
         my $emit = 0;
-        for my $mirrorhash (@$mirrors) {
-            my $mirror = $mirrorhash->{url};
-            my $ok = 0;
-            my $err = 'Timeout';
-            # Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
-            # $ua->head_p($mirror . $filepath)->then(sub {
-            #    my $tx = shift;
-            #    print(STDERR "res\n");
-            #    print(STDERR 'res ' . $tx->result->code . "\n");
-            #    $ok = 1 unless ($tx->result->code < 200 or $tx->result->code > 299);
-            # })->catch(sub {
-            #    my $err1 = shift;
-            #    print(STDERR "catch $err1\n");
-            #    $err = $err1;
-            # })->timeout(1 => 'Timeout!')->wait;
-            # })->wait;
-            my $tx = $ua->head($mirror . $filepath);
-            $ok = 1 unless ($tx->result->code < 200 or $tx->result->code > 299);
-
-            if ($ok) {
-                $c->emit_event('mc_path_hit', {path => $dirname, mirror => $mirror});
-                return $mirror;
-            } else {
-                $emit = 1;
-                # $c->emit_event('mc_mirror_probe', {mirror => $mirror, error => $err, tag => 1});
-                # $c->emit_event('mc_mirror_probe', $dirname);
+        my $recurs1;
+        my $recurs = sub {
+            my $prev = shift;
+            $c->emit_event('mc_mirror_probe', $dirname) if $prev && ($prev == 302) && $emit;
+            return if $prev && $prev == 302;
+            my $mirror = pop @$mirrors;
+            unless ($mirror) {
+                $c->emit_event('mc_mirror_probe', $dirname) if $emit;
+                return $c->mc->root->render_file($c, $filepath);
             }
-        }
-
-        $c->emit_event('mc_mirror_probe', $dirname) if $emit;
-        return undef;
+            my $url = $mirror->{url} . $filepath;
+            my $code;
+            $ua->head_p($url)->then(sub {
+                $code = shift->result->code;
+                if ($code == 200) {
+                    $c->emit_event('mc_path_hit', {path => $dirname, mirror => $url});
+                    return $c->redirect_to($url);
+                }
+                $emit = 1;
+                # $c->emit_event('mc_mirror_probe', {mirror => $url, error => $code}, 1);
+            })->catch(sub {
+                # $c->emit_event('mc_mirror_probe', {mirror => $url, error => shift}, 1);
+                $emit = 1;
+            })->finally(sub {
+                return $recurs1->($code);
+            })->wait;
+        };
+        $recurs1 = $recurs;
+        $recurs->();
     });
+
     return $app;
 }
 
