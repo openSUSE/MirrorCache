openSUSE/MirrorCache

commit 71ba685b44d85809c20a023430dd86d783e9dc34

Stat: consider metalink is a hit

lib/MirrorCache/WebAPI/Plugin/RenderFileFromMirror.pm
@@ -34,7 +34,6 @@ sub register {
         my ($c, $filepath)= @_;
         $c->emit_event('mc_dispatch', $filepath);
         my $root = $c->mc->root;
-        my $mirror = "";
         my $f = Mojo::File->new($filepath);
         my $dirname  = $f->dirname;
         my $basename = $f->basename;
@@ -65,6 +64,9 @@ sub register {
         $ipv = 'ipv6' unless $dm->is_ipv4;
         my $ip = $dm->ip;
         my $mirrors = $c->schema->resultset('Server')->mirrors_country($country, $folder->id, $file->id, $scheme, $ipv, $dm->lat, $dm->lng);
+        unless (@$mirrors) {
+            return $root->render_file($c, $filepath);
+        }
 
         my $headers = $c->req->headers;
         my $accept;
@@ -74,7 +76,9 @@ sub register {
                 my $url = $c->req->url->to_abs;
                 my $origin = $url->scheme . '://' . $url->host;
                 my $xml = _build_metalink($folder->path, $basename, 0, $country, $mirrors, $origin, 'MirrorCache');
-                return $c->render(data => $xml, format => 'xml');
+                $c->render(data => $xml, format => 'xml');
+                $c->stat->redirect_to_mirror($mirrors->[0]->{mirror_id});
+                return 1;
             }
         }
         my $ua  = Mojo::UserAgent->new;

commit 4f4a9dc27c711169cc5bbd7f4a69250ee0010309

Properly record mirror_miss event
The problem was introduced in 71ba685b

lib/MirrorCache/WebAPI/Plugin/RenderFileFromMirror.pm
@@ -65,6 +65,7 @@ sub register {
         my $ip = $dm->ip;
         my $mirrors = $c->schema->resultset('Server')->mirrors_country($country, $folder->id, $file->id, $scheme, $ipv, $dm->lat, $dm->lng);
         unless (@$mirrors) {
+            $c->emit_event('mc_mirror_miss', {path => $dirname, country => $country});
             return $root->render_file($c, $filepath);
         }
 

t/environs/02-files.sh
@@ -82,12 +82,15 @@ test 4 == $(pg9*/sql.sh -t -c "select count(*) from folder_diff_server" mc_test)
 
 cnt="$(pg9*/sql.sh -t -c "select count(*) from audit_event" mc_test)"
 
-curl -Is http://127.0.0.1:3190/download/folder2/file4.dat | grep 200
 curl -Is http://127.0.0.1:3190/download/folder1/file2.dat | grep 302
 
 # it shouldn't try to reach file on mirrors yet, because scanner didn't find files
-test 0 == $(pg9*/sql.sh -t -c "select count(*) from audit_event where name like 'mirror_%error' and id > $cnt" mc_test)
+test 0 == $(pg9*/sql.sh -t -c "select count(*) from audit_event where name like 'mirror_miss' and id > $cnt" mc_test)
 
+
+curl -Is http://127.0.0.1:3190/download/folder2/file4.dat | grep 200
+# now an error must be logged 
+test 1 == $(pg9*/sql.sh -t -c "select count(*) from audit_event where name like 'mirror_miss' and id > $cnt" mc_test)
 
 ##################################
 # let's test path distortions 