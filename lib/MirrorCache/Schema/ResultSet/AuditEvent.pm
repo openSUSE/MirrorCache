d317cc1437e1d885ffc94600a5bc3d02a00b1499
Elisei Roca <eroca@suse.de>
2021-06-08 09:47:50 +0200

    Cleanup: delete rows from audit_event table
    Storage duration for the current event types is hard-coded for now.
    Other event types or new event types get 14 days by default.

--- lib/MirrorCache/Schema/ResultSet/AuditEvent.pm
+++ lib/MirrorCache/Schema/ResultSet/AuditEvent.pm
@@ -18,6 +18,10 @@ package MirrorCache::Schema::ResultSet::AuditEvent;
 use strict;
 use warnings;
 
+use Time::Piece;
+use Time::Seconds;
+use Time::ParseDate;
+
 use base 'DBIx::Class::ResultSet';
 use Mojo::JSON qw/decode_json/;
 
@@ -131,6 +135,96 @@ sub mirror_misses {
     }
     my @country_list = (keys %countries);
     return ($id, \%path_country, \@country_list);
+}
+
+sub cleanup_audit_events {
+    my ($self, $app) = @_;
+
+    my $fail_count;
+    my $last_warning;
+
+    my $rsource = $self->result_source;
+    my $dbh     = $rsource->schema->storage->dbh;
+
+    my @queries;
+    my $other_time_constraint;
+
+    my %event_patterns = (
+        # system events
+        startup => 'startup',
+        path    => 'path_%',
+        mirror  => 'mirror_%',
+        error   => 'error_%',
+        # user events from the UI
+        user   => 'user_%',
+        server => 'server_%',
+    );
+    # duration in days
+    my %storage_durations = (
+        # system events
+        startup => 14,
+        path    => 14,
+        mirror  => 14,
+        error   => 14,
+        # user events from the UI
+        user   => 90,
+        server => 90,
+        # events not defined above
+        other => 14,
+    );
+
+    for my $event_category (keys %storage_durations) {
+        my $duration_in_days = $storage_durations{$event_category};
+        next unless $duration_in_days;
+
+        my $time_constraint = parsedate("$duration_in_days days ago", PREFER_PAST => 1, DATE_REQUIRED => 1);
+        if (!$time_constraint) {
+            $app->log->warn(
+                "Ignoring invalid storage duration '$duration_in_days' for audit event type '$event_category'.");
+            next;
+        }
+        $time_constraint = localtime($time_constraint)->ymd;
+
+        if ($event_category eq 'other') {
+            $other_time_constraint = $time_constraint;
+            next;
+        }
+
+        my $event_pattern = $event_patterns{$event_category};
+        if (!$event_pattern) {
+            $app->log->warn("Ignoring unknown event type '$event_category'.");
+            next;
+        }
+        push(@queries, {name => {-like => $event_pattern}, dt => {'<' => $time_constraint}});
+    }
+
+    for my $query (@queries) {
+        eval {
+            $self->search($query, {order_by => 'dt', rows => 100000})->delete;
+            1;
+        } or do {
+            $fail_count++;
+            $last_warning = $@;
+            $app->log->warn("Cleanup of audit events failed: $@");
+        }
+    }
+
+    if ($other_time_constraint) {
+        my @pattern_values = values %event_patterns;
+        my $sql_other      = 'delete from audit_event where id in (';
+        $sql_other = "$sql_other select id from audit_event where dt < ? and";
+        $sql_other = "$sql_other " . join(' and ', map { 'name not like ?' } @pattern_values);
+        $sql_other = "$sql_other order by dt limit 100000 )";
+        eval {
+            $dbh->prepare($sql_other)->execute($other_time_constraint, @pattern_values);
+            1;
+        } or do {
+            $fail_count++;
+            $last_warning = $@;
+            $app->log->warn("Cleanup of audit events failed: $@");
+        }
+    }
+    return ($fail_count, $last_warning) if $fail_count;
 }
 
 1;
