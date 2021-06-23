feb23c79785d8006517b1a1e30a9da6b99f4f1bd
Andrii Nikitin <anikitin@suse.de>
2021-06-23 05:57:35 +0200

    Fix url for task MirrorCheckFromStat when metalink

--- t/environ/09-stability-02-mirror-check.sh
+++ t/environ/09-stability-02-mirror-check.sh
@@ -29,8 +29,21 @@ $mc/curl -I /download/folder1/file1.dat | grep -C20 302 | grep $($ap7/print_addr
 
 # now remove the file shut down ap7 and call mirror_check
 rm $ap7/dt/folder1/file1.dat
-$mc/backstage/job mirror_check_from_stat
-$mc/backstage/shoot
 
-# no download anymore
+id=$($mc/backstage/job mirror_check_from_stat)
+$mc/backstage/shoot
+# check a new mirror_scan job was scheduled
+test 1 == $($mc/db/sql "select count(*) from minion_jobs where id>$id and task = 'mirror_scan'")
+
+# no redirect anymore
 $mc/curl -I /download/folder1/file1.dat | grep -C20 '200 OK'
+
+# restore deleted file from mirrror
+touch $ap7/dt/folder1/file1.dat
+# check metalinks are handled properly
+$mc/curl -I /download/folder1/file2.dat.metalink | grep -C20 '200 OK'
+id=$($mc/backstage/job mirror_check_from_stat)
+
+$mc/backstage/shoot
+# check no new mirror_scan job was scheduled
+test 0 == $($mc/db/sql "select count(*) from minion_jobs where id>$id and task = 'mirror_scan'")
