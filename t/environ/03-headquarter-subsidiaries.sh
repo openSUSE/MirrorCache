62c5c2b029b7d6e4746a4cc73a7de92f495b01ee
Andrii Nikitin <46994839+andrii-suse@users.noreply.github.com>
2021-07-06 00:00:20 +0200

    Fix render greeting on landing page when TOP_FOLDERS is set (#156)
    [skip ci]
    * ui: Render greeting on the root dir
    * Fix dsn string when MIRRORCACHE_DBHOST is set
    * Fix wrong redirect of landing page to a subsidiary

--- t/environ/03-headquarter-subsidiaries.sh
+++ t/environ/03-headquarter-subsidiaries.sh
@@ -29,6 +29,7 @@ $mc9/db/sql "insert into subsidiary(hostname,region) select '$na_address','na'"
 $mc9/db/sql "insert into subsidiary(hostname,region) select '$eu_address','eu'"
 $mc9/db/sql "insert into subsidiary(hostname,region) select '$as_address','as'"
 
+$mc9/gen_env MIRRORCACHE_TOP_FOLDERS='folder1 folder2 folder3'
 $mc9/start
 $mc6/gen_env MIRRORCACHE_REGION=na MIRRORCACHE_HEADQUARTER=$hq_address
 $mc6/start
@@ -37,18 +38,25 @@ $mc7/start
 $mc8/gen_env MIRRORCACHE_REGION=as MIRRORCACHE_HEADQUARTER=$hq_address
 $mc8/start
 
+echo the root folder is not redirected
+curl --interface $eu_interface -Is http://$hq_address/ | grep '200 OK'
+
+echo check redirection from headquarter
 curl --interface $na_interface -Is http://$hq_address/download/folder1/file1.dat | grep "Location: http://$na_address/download/folder1/file1.dat"
 curl --interface $eu_interface -Is http://$hq_address/download/folder1/file1.dat | grep "Location: http://$eu_address/download/folder1/file1.dat"
 curl --interface $as_interface -Is http://$hq_address/download/folder1/file1.dat | grep "Location: http://$as_address/download/folder1/file1.dat"
 
+echo check redirection from na
 curl --interface $na_interface -Is http://$na_address/download/folder1/file1.dat | grep '200 OK'
 curl --interface $eu_interface -Is http://$na_address/download/folder1/file1.dat | grep "Location: http://$hq_address/download/folder1/file1.dat"
 curl --interface $as_interface -Is http://$na_address/download/folder1/file1.dat | grep "Location: http://$hq_address/download/folder1/file1.dat"
 
+echo check redirection from eu
 curl --interface $na_interface -Is http://$eu_address/download/folder1/file1.dat | grep "Location: http://$hq_address/download/folder1/file1.dat"
 curl --interface $eu_interface -Is http://$eu_address/download/folder1/file1.dat | grep '200 OK'
 curl --interface $as_interface -Is http://$eu_address/download/folder1/file1.dat | grep "Location: http://$hq_address/download/folder1/file1.dat"
 
+echo check redirection from as
 curl --interface $na_interface -Is http://$as_address/download/folder1/file1.dat | grep "Location: http://$hq_address/download/folder1/file1.dat"
 curl --interface $eu_interface -Is http://$as_address/download/folder1/file1.dat | grep "Location: http://$hq_address/download/folder1/file1.dat"
 curl --interface $as_interface -Is http://$as_address/download/folder1/file1.dat | grep '200 OK'
@@ -57,7 +65,7 @@ curl --interface $as_interface -Is http://$as_address/download/folder1/file1.dat
 curl --interface $as_interface -Is http://$as_address/download/folder1/file1.dat?REGION=na | grep "Location: http://$hq_address/download/folder1/file1.dat"
 curl --interface $as_interface -Is "http://$as_address/download/folder1/file1.dat?COUNTRY=cn&REGION=na" | grep "Location: http://$hq_address/download/folder1/file1.dat"
 
-# check non-download routers shouldn't be redirected
+echo check non-download routers shouldnt be redirected
 curl --interface $na_interface -Is http://$hq_address/rest/server | grep '200 OK'
 curl --interface $eu_interface -Is http://$hq_address/rest/server | grep '200 OK'
 curl --interface $as_interface -Is http://$hq_address/rest/server | grep '200 OK'
