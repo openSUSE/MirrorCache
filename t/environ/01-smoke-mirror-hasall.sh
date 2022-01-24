a3244017bc840aa8c967c977127c94a26dedc5db
Andrii Nikitin <anikitin@suse.de>
2022-01-24 19:30:48 +0100

    t: Fix test after domain motexist.com started returning 200 instead of failure

--- t/environ/01-smoke-mirror-hasall.sh
+++ t/environ/01-smoke-mirror-hasall.sh
@@ -20,13 +20,14 @@ $ap7/curl /folder1/ | grep file1.1.dat
 $ap8/start
 $ap8/curl /folder1/ | grep file1.1.dat
 
+FAKEURL="notexists${RANDOM}.com"
 
 $mc/db/sql "insert into server(hostname,urldir,enabled,country,region) select '$($ap7/print_address)','','t','us','na'"
 $mc/db/sql "insert into server(hostname,urldir,enabled,country,region) select '$($ap8/print_address)','','t','de','eu'"
-$mc/db/sql "insert into server(hostname,urldir,enabled,country,region) select 'notexists.com','','t','it','eu'"
+$mc/db/sql "insert into server(hostname,urldir,enabled,country,region) select '$FAKEURL','','t','it','eu'"
 
 
-$mc/db/sql "insert into server_capability_declaration(server_id, capability, enabled) select id, 'hasall', 't' from server where hostname = 'notexists.com'";
+$mc/db/sql "insert into server_capability_declaration(server_id, capability, enabled) select id, 'hasall', 't' from server where hostname = '${FAKEURL}'";
 
 $mc/curl -Is /download/folder1/file1.1.dat.mirrorlist
 
@@ -36,10 +37,10 @@ $mc/backstage/shoot
 $mc/backstage/job mirror_scan_schedule
 $mc/backstage/shoot
 
-$mc/curl -I /download/folder1/file1.1.dat?COUNTRY=it | grep -C10 302 | grep "notexists.com"
-$mc/curl /download/folder1/file1.1.dat.mirrorlist | grep "notexists.com"
+$mc/curl -I /download/folder1/file1.1.dat?COUNTRY=it | grep -C10 302 | grep "${FAKEURL}"
+$mc/curl /download/folder1/file1.1.dat.mirrorlist | grep "${FAKEURL}"
 # with pedantic we ignore it though
 rc=0
-$mc/curl -I /download/folder1/file1.1.dat?"COUNTRY=it&PEDANTIC=1" | grep "notexists.com" || rc=$?
+$mc/curl -I /download/folder1/file1.1.dat?"COUNTRY=it&PEDANTIC=1" | grep "${FAKEURL}" || rc=$?
 test $rc -gt 0
 echo success
