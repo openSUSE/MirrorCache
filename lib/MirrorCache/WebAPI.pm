db9b761eefee1ae6110a59fe4f7da9b383f65ce6
Andrii Nikitin <anikitin@suse.de>
2022-03-23 18:19:22 +0100

    Move initialization code from before_server_start to execute earlier
    This way commands will return correct exit code when initialization failed (e.g. rcmirrorcache start)

--- lib/MirrorCache/WebAPI.pm
+++ lib/MirrorCache/WebAPI.pm
@@ -21,10 +21,32 @@ use Mojolicious::Commands;
 use MirrorCache::Schema;
 use MirrorCache::Utils 'random_string';
 
+has 'version';
+has 'root';
+has '_geodb';
+
 sub new {
     my $self = shift->SUPER::new;
     # setting pid_file in startup will not help, need to set it earlier
     $self->config->{hypnotoad}{pid_file} = $ENV{MIRRORCACHE_HYPNOTOAD_PID} // '/run/mirrorcache/hypnotoad.pid';
+
+    # I wasn't able to find reliable way in Mojolicious to detect when WebUI is started
+    # (e.g. in daemon or hypnotoad in contrast to other commands like backend start / shoot)
+    # so the code below tries to detect if _setup_ui is needed to be called
+
+    my $started = 0;
+    for (my $i = 0; my @r = caller($i); $i++) {
+        next unless $r[3] =~ m/Hypnotoad/;
+        $self->_setup_webui;
+        $started = 1;
+        last;
+    }
+
+    $self->hook(before_command => sub {
+        my ($command, $arg) = @_;
+        $self->_setup_webui if ref($command) =~ m/daemon|prefork/; 
+    }) unless $started;
+
     $self;
 }
 
@@ -45,7 +67,7 @@ sub startup {
     eval {
         MirrorCache::Schema->singleton->migrate();
         1;
-    } or die("Automatic migration failed: $@\nFix table structure and insert into mojo_migrations select 'mirrorcache',version");
+    } or die("Automatic migration failed: $@\nFix table structure and insert into mojo_migrations select 'mirrorcache', version");
     my $secret = random_string(16);
     $self->config->{hypnotoad}{listen}   = [$ENV{MOJO_LISTEN} // 'http://*:8080'];
     $self->config->{hypnotoad}{proxy}    = $ENV{MOJO_REVERSE_PROXY} // 0,
@@ -61,89 +83,9 @@ sub startup {
     my $current_version = $self->detect_current_version() || "unknown";
     $self->defaults(current_version => $current_version);
     $self->log->info("initializing $current_version");
+    $self->version($current_version);
 
     $self->defaults(branding => $ENV{MIRRORCACHE_BRANDING});
-
-    $self->app->hook(before_server_start => sub {
-        die("MIRRORCACHE_ROOT is not set") unless $root;
-        if ((-1 == rindex($root, 'http', 0)) && (-1 == rindex($root, 'rsync://', 0)) ) {
-            my $i = index($root, ':');
-            my $dir = ($i > -1 ? substr($root, 0, $i) : $root);
-            die("MIRRORCACHE_ROOT is not a directory ($root)") unless -d $dir;
-        }
-
-        # Optional initialization with access to the app
-        my $r = $self->routes->namespaces(['MirrorCache::WebAPI::Controller']);
-        $r->get('/favicon.ico' => sub { my $c = shift; $c->render_static('favicon.ico') });
-
-        $r->get('/version')->to(cb => sub {
-            shift->render(text => $current_version);
-        }) if $current_version;
-        $r->post('/session')->to('session#create');
-        $r->delete('/session')->to('session#destroy');
-        $r->get('/login')->name('login')->to('session#create');
-        $r->post('/login')->to('session#create');
-        $r->post('/logout')->name('logout')->to('session#destroy');
-        $r->get('/response')->to('session#response');
-        $r->post('/response')->to('session#response');
-
-        my $rest = $r->any('/rest');
-        my $rest_r    = $rest->any('/')->to(namespace => 'MirrorCache::WebAPI::Controller::Rest');
-        $rest_r->get('/server')->name('rest_server')->to('table#list', table => 'Server');
-        $rest_r->get('/server/:id')->to('table#list', table => 'Server');
-        $rest_r->get('/project/:name')->to('project#show');
-        $rest_r->get('/project/:name/mirror_summary')->to('project#mirror_summary');
-        $rest_r->get('/project/:name/mirror_list')->to('project#mirror_list');
-
-        my $rest_operator_auth;
-        $rest_operator_auth = $rest->under('/')->to('session#ensure_operator');
-        my $rest_operator_r = $rest_operator_auth->any('/')->to(namespace => 'MirrorCache::WebAPI::Controller::Rest');
-        $rest_operator_r->post('/server')->to('table#create', table => 'Server');
-        $rest_operator_r->post('/server/:id')->name('post_server')->to('table#update', table => 'Server');
-        $rest_operator_r->delete('/server/:id')->to('table#destroy', table => 'Server');
-        $rest_operator_r->put('/server/location/:id')->name('rest_put_server_location')->to('server_location#update_location');
-        $rest_operator_r->post('/sync_tree')->name('rest_post_sync_tree')->to('folder_jobs#sync_tree');
-
-        $rest_r->get('/folder')->name('rest_folder')->to('table#list', table => 'Folder');
-
-        $rest_r->get('/folder_jobs/:id')->name('rest_folder_jobs')->to('folder_jobs#list');
-        $rest_r->get('/myip')->name('rest_myip')->to('my_ip#show') if $geodb;
-
-        $rest_r->get('/stat')->name('rest_stat')->to('stat#list');
-        $rest_r->get('/mystat')->name('rest_mystat')->to('stat#mylist');
-
-        my $app_r = $r->any('/app')->to(namespace => 'MirrorCache::WebAPI::Controller::App');
-
-        $app_r->get('/server')->name('server')->to('server#index');
-        $app_r->get('/folder')->name('folder')->to('folder#index');
-        $app_r->get('/folder/<id:num>')->name('folder_show')->to('folder#show');
-
-        my $admin = $r->any('/admin');
-        my $admin_auth = $admin->under('/')->to('session#ensure_admin')->name('ensure_admin');
-        my $admin_r = $admin_auth->any('/')->to(namespace => 'MirrorCache::WebAPI::Controller::Admin');
-
-        $admin_r->delete('/folder/<id:num>')->to('folder#delete_cascade');
-        $admin_r->delete('/folder_diff/<id:num>')->to('folder#delete_diff');
-
-        $admin_r->get('/user')->name('get_user')->to('user#index');
-        $admin_r->post('/user/:userid')->name('post_user')->to('user#update');
-
-        my $rest_user_r = $admin_auth->any('/')->to(namespace => 'MirrorCache::WebAPI::Controller::Rest');
-        $rest_user_r->delete('/user/<id:num>')->name('delete_user')->to('user#delete');
-
-        $admin_r->get('/auditlog')->name('audit_log')->to('audit_log#index');
-        $admin_r->get('/auditlog/ajax')->name('audit_ajax')->to('audit_log#ajax');
-
-        $r->get('/index' => sub { shift->render('main/index') });
-        $r->get('/' => sub { shift->render('main/index') })->name('index');
-
-        $self->plugin(AssetPack => {pipes => [qw(Sass Css JavaScript Fetch Combine)]});
-        $self->asset->process;
-        $self->plugin('Stat');
-        $self->plugin('Dir');
-        $self->log->info("server started:  $current_version");
-    });
-
 
     $self->plugin('RenderFile');
 
@@ -170,6 +112,7 @@ sub startup {
     else {
         $self->plugin('Mmdb', { reader => $geodb });
     }
+    $self->_geodb($geodb) if $geodb;
 
     $self->plugin('Helpers', root => $root, route => '/download');
     $self->plugin('Subsidiary');
@@ -182,7 +125,91 @@ sub startup {
         } else {
             $self->plugin('RootRemote');
         }
+        $self->root($root)
     }
+}
+
+sub _setup_webui {
+    my $self = shift;
+    my $root = $self->root;
+    die("MIRRORCACHE_ROOT is not set") unless $root;
+    if ((-1 == rindex($root, 'http', 0)) && (-1 == rindex($root, 'rsync://', 0)) ) {
+        my $i = index($root, ':');
+        my $dir = ($i > -1 ? substr($root, 0, $i) : $root);
+        die("MIRRORCACHE_ROOT is not a directory ($root)") unless -d $dir;
+    }
+
+    # Optional initialization with access to the app
+    my $r = $self->routes->namespaces(['MirrorCache::WebAPI::Controller']);
+    $r->get('/favicon.ico' => sub { my $c = shift; $c->render_static('favicon.ico') });
+
+    my $current_version = $self->version;
+    $r->get('/version')->to(cb => sub {
+        shift->render(text => $current_version);
+    }) if $current_version;
+    $r->post('/session')->to('session#create');
+    $r->delete('/session')->to('session#destroy');
+    $r->get('/login')->name('login')->to('session#create');
+    $r->post('/login')->to('session#create');
+    $r->post('/logout')->name('logout')->to('session#destroy');
+    $r->get('/response')->to('session#response');
+    $r->post('/response')->to('session#response');
+
+    my $rest = $r->any('/rest');
+    my $rest_r    = $rest->any('/')->to(namespace => 'MirrorCache::WebAPI::Controller::Rest');
+    $rest_r->get('/server')->name('rest_server')->to('table#list', table => 'Server');
+    $rest_r->get('/server/:id')->to('table#list', table => 'Server');
+    $rest_r->get('/project/:name')->to('project#show');
+    $rest_r->get('/project/:name/mirror_summary')->to('project#mirror_summary');
+    $rest_r->get('/project/:name/mirror_list')->to('project#mirror_list');
+
+    my $rest_operator_auth;
+    $rest_operator_auth = $rest->under('/')->to('session#ensure_operator');
+    my $rest_operator_r = $rest_operator_auth->any('/')->to(namespace => 'MirrorCache::WebAPI::Controller::Rest');
+    $rest_operator_r->post('/server')->to('table#create', table => 'Server');
+    $rest_operator_r->post('/server/:id')->name('post_server')->to('table#update', table => 'Server');
+    $rest_operator_r->delete('/server/:id')->to('table#destroy', table => 'Server');
+    $rest_operator_r->put('/server/location/:id')->name('rest_put_server_location')->to('server_location#update_location');
+    $rest_operator_r->post('/sync_tree')->name('rest_post_sync_tree')->to('folder_jobs#sync_tree');
+
+    $rest_r->get('/folder')->name('rest_folder')->to('table#list', table => 'Folder');
+
+    $rest_r->get('/folder_jobs/:id')->name('rest_folder_jobs')->to('folder_jobs#list');
+    $rest_r->get('/myip')->name('rest_myip')->to('my_ip#show') if $self->_geodb;
+
+    $rest_r->get('/stat')->name('rest_stat')->to('stat#list');
+    $rest_r->get('/mystat')->name('rest_mystat')->to('stat#mylist');
+
+    my $app_r = $r->any('/app')->to(namespace => 'MirrorCache::WebAPI::Controller::App');
+
+    $app_r->get('/server')->name('server')->to('server#index');
+    $app_r->get('/folder')->name('folder')->to('folder#index');
+    $app_r->get('/folder/<id:num>')->name('folder_show')->to('folder#show');
+
+    my $admin = $r->any('/admin');
+    my $admin_auth = $admin->under('/')->to('session#ensure_admin')->name('ensure_admin');
+    my $admin_r = $admin_auth->any('/')->to(namespace => 'MirrorCache::WebAPI::Controller::Admin');
+
+    $admin_r->delete('/folder/<id:num>')->to('folder#delete_cascade');
+    $admin_r->delete('/folder_diff/<id:num>')->to('folder#delete_diff');
+
+    $admin_r->get('/user')->name('get_user')->to('user#index');
+    $admin_r->post('/user/:userid')->name('post_user')->to('user#update');
+
+    my $rest_user_r = $admin_auth->any('/')->to(namespace => 'MirrorCache::WebAPI::Controller::Rest');
+    $rest_user_r->delete('/user/<id:num>')->name('delete_user')->to('user#delete');
+
+    $admin_r->get('/auditlog')->name('audit_log')->to('audit_log#index');
+    $admin_r->get('/auditlog/ajax')->name('audit_ajax')->to('audit_log#ajax');
+
+    $r->get('/index' => sub { shift->render('main/index') });
+    $r->get('/' => sub { shift->render('main/index') })->name('index');
+
+    $self->plugin(AssetPack => {pipes => [qw(Sass Css JavaScript Fetch Combine)]});
+    $self->asset->process;
+    $self->plugin('Stat');
+    $self->plugin('Dir');
+    $self->log->info("server started:  $current_version");
 }
 
 sub detect_current_version() {
