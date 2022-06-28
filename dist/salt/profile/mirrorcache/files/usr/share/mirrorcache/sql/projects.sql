7a53cce6c66979c981db4d9e86af3148edc48596
Andrii Nikitin <46994839+andrii-suse@users.noreply.github.com>
2022-06-28 12:42:31 +0200

    dist: Fix project paths in projects.sql
    [ci skip]

--- dist/salt/profile/mirrorcache/files/usr/share/mirrorcache/sql/projects.sql
+++ dist/salt/profile/mirrorcache/files/usr/share/mirrorcache/sql/projects.sql
@@ -9,18 +9,18 @@ insert into project(name,path) select '15.2 ISO','/distribution/leap/15.2/iso';
 insert into project(name,path) select '15.2 repo','/distribution/leap/15.2/repo';
 insert into project(name,path) select '15.2 debug','/debug/distribution/leap/15.2/repo';
 insert into project(name,path) select '15.2 source','/source/distribution/leap/15.2/repo';
-insert into project(name,path) select '15.2 update','/update/distribution/leap/15.2/repo';
+insert into project(name,path) select '15.2 update','/update/distribution/leap/15.2';
 
 insert into project(name,path) select '15.3 ISO','/distribution/leap/15.3/iso';
 insert into project(name,path) select '15.3 repo','/distribution/leap/15.3/repo';
 insert into project(name,path) select '15.3 debug','/debug/distribution/leap/15.3/repo';
 insert into project(name,path) select '15.3 source','/source/distribution/leap/15.3/repo';
-insert into project(name,path) select '15.3 update','/update/distribution/leap/15.3/repo';
-insert into project(name,path) select '15.3 port','/port/aarch64/distribution/leap/15.3';
+insert into project(name,path) select '15.3 update','/update/distribution/leap/15.3';
+insert into project(name,path) select '15.3 port','/ports/aarch64/distribution/leap/15.3';
 
 insert into project(name,path) select '15.4 ISO','/distribution/leap/15.4/iso';
 insert into project(name,path) select '15.4 repo','/distribution/leap/15.4/repo';
 insert into project(name,path) select '15.4 debug','/debug/distribution/leap/15.4/repo';
 insert into project(name,path) select '15.4 source','/source/distribution/leap/15.4/repo';
-insert into project(name,path) select '15.4 update','/update/distribution/leap/15.4/repo';
-insert into project(name,path) select '15.4 port','/port/aarch64/distribution/leap/15.4';
+insert into project(name,path) select '15.4 update','/update/distribution/leap/15.4';
+insert into project(name,path) select '15.4 port','/ports/aarch64/distribution/leap/15.4';
