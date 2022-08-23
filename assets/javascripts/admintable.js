4e5fa33569afe91321ec1c6a7433b0f8f8008bdb
Andrii Nikitin <anikitin@suse.de>
2022-08-23 12:31:56 +0200

    Add column sponsor to server table

--- assets/javascripts/admintable.js
+++ assets/javascripts/admintable.js
@@ -130,7 +130,7 @@ function getAdminTableRowData(trElement, dataToSubmit, internalRowData) {
     var tableHeadings = trElement.closest('table').find('th');
     trElement.find('td').each(function() {
         var th = tableHeadings.eq(this.cellIndex);
-        var name = th.text().trim().toLowerCase();
+        var name = th.text().trim().toLowerCase().replace(/ /g, '_');
         var value;
         if (th.hasClass("col_value")) {
             value = $(this).find("input").val();
@@ -362,7 +362,7 @@ function setupAdminTable(editable) {
         if (th.hasClass('col_action')) {
             columnName = 'id';
         } else {
-            columnName = th.text().trim().toLowerCase();
+            columnName = th.text().trim().toLowerCase().replace(/ /g, '_');
         }
         columns.push({ data: columnName });
 
--- lib/MirrorCache/Schema/Result/Server.pm
+++ lib/MirrorCache/Schema/Result/Server.pm
@@ -1,93 +1,12 @@
 use utf8;
 package MirrorCache::Schema::Result::Server;
 
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
-
-=head1 NAME
-
-MirrorCache::Schema::Result::Server
-
-=cut
-
 use strict;
 use warnings;
 
 use base 'DBIx::Class::Core';
 
-=head1 TABLE: C<server>
-
-=cut
-
 __PACKAGE__->table("server");
-
-=head1 ACCESSORS
-
-=head2 id
-
-  data_type: 'integer'
-  is_auto_increment: 1
-  is_nullable: 0
-  sequence: 'server_id_seq'
-
-=head2 hostname
-
-  data_type: 'varchar'
-  is_nullable: 0
-  size: 128
-
-=head2 urldir
-
-  data_type: 'varchar'
-  is_nullable: 0
-  size: 128
-
-=head2 enabled
-
-  data_type: 'boolean'
-  is_nullable: 0
-
-=head2 region
-
-  data_type: 'varchar'
-  is_nullable: 0
-  size: 2
-
-=head2 country
-
-  data_type: 'varchar'
-  is_nullable: 0
-  size: 2
-
-=head2 score
-
-  data_type: 'smallint'
-  is_nullable: 0
-
-=head2 comment
-
-  data_type: 'text'
-  is_nullable: 0
-
-=head2 public_notes
-
-  data_type: 'varchar'
-  is_nullable: 0
-  size: 512
-
-=head2 lat
-
-  data_type: 'numeric'
-  is_nullable: 1
-  size: [6,3]
-
-=head2 lng
-
-  data_type: 'numeric'
-  is_nullable: 1
-  size: [6,3]
-
-=cut
 
 __PACKAGE__->add_columns(
   "id",
@@ -97,6 +16,10 @@ __PACKAGE__->add_columns(
     is_nullable       => 0,
     sequence          => "server_id_seq",
   },
+  "sponsor",
+  { data_type => "varchar", is_nullable => 1, size => 64 },
+  "sponsor_url",
+  { data_type => "varchar", is_nullable => 1, size => 64 },
   "hostname",
   { data_type => "varchar", is_nullable => 0, size => 128 },
   "urldir",
@@ -119,27 +42,7 @@ __PACKAGE__->add_columns(
   { data_type => "numeric", is_nullable => 1, size => [6, 3] },
 );
 
-=head1 PRIMARY KEY
-
-=over 4
-
-=item * L</id>
-
-=back
-
-=cut
-
 __PACKAGE__->set_primary_key("id");
-
-=head1 RELATIONS
-
-=head2 folder_diff_file_servers
-
-Type: has_many
-
-Related object: L<MirrorCache::Schema::Result::FolderDiffFileServer>
-
-=cut
 
 __PACKAGE__->has_many(
   "folder_diff_file_servers",
