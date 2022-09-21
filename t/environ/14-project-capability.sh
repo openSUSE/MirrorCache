7e409d4c17c44364ab03497873635e70b40a54b7
Andrii Nikitin <anikitin@suse.de>
2022-09-21 10:53:40 +0200

    Fix mirror picked despite disabled in server_project table

--- t/environ/14-project-capability.sh
+++ t/environ/14-project-capability.sh
@@ -51,6 +51,10 @@ $mc/backstage/shoot
 $mc/sql_test 0 == "select state from server_project where server_id = 2 and project_id = 1"
 $mc/sql_test -1 == "select state from server_project where server_id = 3 and project_id = 1"
 
+echo metalink doesnt have ap8, because project was disabled on server
+rc=0
+$mc/curl /download/project1/folder1/file.1.1.dat.metalink | grep $($ap8/print_address) || rc=$?
+test $rc -gt 0
 
 $mc/sql "update server_project set state = 0 where server_id = 3 and project_id = 1"
 $mc/backstage/job -e mirror_probe_projects
@@ -58,5 +62,16 @@ $mc/backstage/shoot
 $mc/sql_test 0 == "select state from server_project where server_id = 2 and project_id = 1"
 $mc/sql_test 1 == "select state from server_project where server_id = 3 and project_id = 1"
 
+$mc/backstage/job -e mirror_scan -a '["/project1/folder1"]'
+$mc/backstage/shoot
+
+echo now metalink has ap8, because project has been enabled on server_id 3
+$mc/curl /download/project1/folder1/file1.1.dat.metalink | grep $($ap8/print_address)
+
+echo disable again, make sure it disappeared in mirrorlist
+$mc/sql "update server_project set state = 0 where server_id = 3 and project_id = 1"
+rc=0
+$mc/curl /download/project1/folder1/file1.1.dat.metalink | grep $($ap8/print_address) || rc=$?
+test $rc -gt 0
 
 echo success
