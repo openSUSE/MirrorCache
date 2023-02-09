ff67753d04f71d133009f37234f16501f2dd1c17
Andrii Nikitin <anikitin@suse.de>
2023-02-09 09:49:36 +0100

    Add leap 15.5 to project list
    [ci skip]

--- dist/salt/profile/mirrorcache/files/usr/share/mirrorcache/sql/projects.sql
+++ dist/salt/profile/mirrorcache/files/usr/share/mirrorcache/sql/projects.sql
@@ -5,22 +5,24 @@ insert into project(name,path) select 'TW debug','/debug/tumbleweed/repo';
 insert into project(name,path) select 'TW source','/source/tumbleweed/repo';
 insert into project(name,path) select 'TW history','/history';
 
-insert into project(name,path) select '15.2 ISO','/distribution/leap/15.2/iso';
-insert into project(name,path) select '15.2 repo','/distribution/leap/15.2/repo';
-insert into project(name,path) select '15.2 debug','/debug/distribution/leap/15.2/repo';
-insert into project(name,path) select '15.2 source','/source/distribution/leap/15.2/repo';
-insert into project(name,path) select '15.2 update','/update/distribution/leap/15.2';
-
 insert into project(name,path) select '15.3 ISO','/distribution/leap/15.3/iso';
 insert into project(name,path) select '15.3 repo','/distribution/leap/15.3/repo';
 insert into project(name,path) select '15.3 debug','/debug/distribution/leap/15.3/repo';
 insert into project(name,path) select '15.3 source','/source/distribution/leap/15.3/repo';
-insert into project(name,path) select '15.3 update','/update/distribution/leap/15.3';
+insert into project(name,path) select '15.3 update','/update/leap/15.3';
 insert into project(name,path) select '15.3 port','/ports/aarch64/distribution/leap/15.3';
 
 insert into project(name,path) select '15.4 ISO','/distribution/leap/15.4/iso';
 insert into project(name,path) select '15.4 repo','/distribution/leap/15.4/repo';
 insert into project(name,path) select '15.4 debug','/debug/distribution/leap/15.4/repo';
 insert into project(name,path) select '15.4 source','/source/distribution/leap/15.4/repo';
-insert into project(name,path) select '15.4 update','/update/distribution/leap/15.4';
+insert into project(name,path) select '15.4 update','/update/leap/15.4';
 insert into project(name,path) select '15.4 port','/ports/aarch64/distribution/leap/15.4';
+
+insert into project(name,path) select '15.5 ISO','/distribution/leap/15.5/iso';
+insert into project(name,path) select '15.5 repo','/distribution/leap/15.5/repo';
+insert into project(name,path) select '15.5 debug','/debug/distribution/leap/15.5/repo';
+insert into project(name,path) select '15.5 source','/source/distribution/leap/15.5/repo';
+insert into project(name,path) select '15.5 update','/update/leap/15.5';
+insert into project(name,path) select '15.5 port','/ports/aarch64/distribution/leap/15.5';
+
