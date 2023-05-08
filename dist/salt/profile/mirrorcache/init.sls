8f4f93b570d455350cd160b102492efaacb14e81
Andrii Nikitin <46994839+andrii-suse@users.noreply.github.com>
2023-05-08 13:35:27 +0200

    rework salt test to use formula (#378)
    [ci skip]

--- dist/salt/profile/mirrorcache/init.sls
+++
@@ -1,4 +0,0 @@
-include:
-  - profile.mirrorcache.packages
-  - profile.mirrorcache.services-init
-  - profile.mirrorcache.services
--- dist/salt/test/test.sh
+++ dist/salt/test/test.sh
@@ -2,8 +2,9 @@ set -ex
 curl -si 127.0.0.1:3000/rest/server || echo "return_code=$?"
 curl -s 127.0.0.1:3000/rest/server | grep -o mirror.23media.com
 
-docker exec -t mirrorcachesalted curl --interface 127.0.0.3 -s 127.0.0.1:3000/download | grep repositories
-docker exec -t mirrorcachesalted curl --interface 127.0.0.3 -sL 127.0.0.1:3000/repositories | grep '<tr>'
+podman exec mirrorcachesalted curl --interface 127.0.0.3 -s 127.0.0.1:3000/download | grep repositories
+podman exec mirrorcachesalted curl --interface 127.0.0.3 -sL 127.0.0.1:3000/repositories | grep '<tr>'
 
-docker exec -t mirrorcachesalted journalctl -xn200 --no-pager -u mirrorcache
-docker exec -t mirrorcachesalted salt-call --local state.apply -l debug 'profile/mirrorcache/services'
+podman exec mirrorcachesalted journalctl -xn200 --no-pager -u mirrorcache
+podman exec mirrorcachesalted salt-call --local state.apply -l debug mirrorcache.webui
+podman exec mirrorcachesalted salt-call --local state.apply -l debug mirrorcache.backstage
